751508af5b486f850912725c9fe7f36cd5d6969f
Kyenn <Kyennlim@gmail.com>
2024-07-06 18:52:27 +0800

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vire-deploy/"
+  base: "/Birthday-to-my-good-friend/"
 })
